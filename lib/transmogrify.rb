04400240ac0499071dabeff0b23e4189c62a390e
Ryan Davis <ryand-ruby@zenspider.com>
2008-11-23 12:46:00 -0800

    Added test_rewrite_lvar and made it pass... we're flying\!

--- lib/transmogrify.rb
+++ lib/transmogrify.rb
@@ -5,4 +5,28 @@ require 'ruby_parser'
 
 class Transmogrify < SexpProcessor
   VERSION = '1.0.0'
+
+  def initialize
+    super
+
+    @lvar_count = 0
+    @lvars = {}
+  end
+
+  def rewrite_lasgn exp
+    name = exp[1]
+    exp[1] = @lvars[name] = new_lvar
+    exp
+  end
+
+  def rewrite_lvar exp
+    name = exp[1]
+    exp[1] = @lvars[name]
+    exp
+  end
+
+  def new_lvar
+    @lvar_count += 1
+    :"douche#{'%02d' % @lvar_count}"
+  end
 end
--- test/test_transmogrify.rb
+++ test/test_transmogrify.rb
@@ -3,7 +3,23 @@ require 'test/unit'
 require 'transmogrify'
 
 class TestTransmogrify < Test::Unit::TestCase
-  def test_truth
-    assert true # omg I can't believe I just wrote this
+  def test_rewrite_lvar
+    s = RubyParser.new.process <<-RUBY
+      number = 1
+      string = 'string'
+      puts number
+      puts string
+    RUBY
+
+    expected = s(:block,
+                 s(:lasgn, :douche01, s(:lit, 1)),
+                 s(:lasgn, :douche02, s(:str, "string")),
+                 s(:call, nil, :puts, s(:arglist, s(:lvar, :douche01))),
+                 s(:call, nil, :puts, s(:arglist, s(:lvar, :douche02))))
+
+    trans = Transmogrify.new
+    actual = trans.process s
+
+    assert_equal expected, actual
   end
 end
