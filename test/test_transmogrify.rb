cbc2561c19fd99b8640695a1f51cf995dcc3031d
Ryan Davis <ryand-ruby@zenspider.com>
2008-11-23 12:25:15 -0800

    First test pass\! UGH

--- test/test_transmogrify.rb
+++ test/test_transmogrify.rb
@@ -1,1 +1,9 @@
-# tests
+
+require 'test/unit'
+require 'transmogrify'
+
+class TestTransmogrify < Test::Unit::TestCase
+  def test_truth
+    assert true # omg I can't believe I just wrote this
+  end
+end
