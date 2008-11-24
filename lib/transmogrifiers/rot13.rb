8d835d67a60a9140613722a916b663a071a82c49
Ryan Davis <ryand-ruby@zenspider.com>
2008-11-23 18:35:12 -0800

    I should have dealt with uppercase, for the camel case dorks

--- lib/transmogrifiers/rot13.rb
+++ lib/transmogrifiers/rot13.rb
@@ -1,7 +1,7 @@
 module Transmogrifiers
   module Rot13
     def transmogrify(name)
-      name.to_s.tr('a-mn-z', 'n-za-m').to_sym
+      name.to_s.tr('a-mn-zA-MN-Z', 'n-za-mN-ZA-M').to_sym
     end
   end
 end
